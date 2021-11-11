bbb05e2269388cb579b38cc9205d8eb703fdeac8
SEFAZ\fabianolira <fab231282aila>
2021-11-11 11:26:21 -0300

    Usuarios modulo 3

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUsersDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({ 
+        name, 
+        email, 
+        password, 
+        driver_licence 
+    }: ICreateUserDTO): Promise<void> {
+
+        await this.usersRepository.create({
+            name,
+            email,
+            password,
+            driver_licence
+        });
+    }
+}
+
+export { CreateUserUseCase }
