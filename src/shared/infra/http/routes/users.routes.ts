df9152efe68af2550cee10403cfe04b533950ef1
SEFAZ\fabianolira <fab231282aila>
2021-12-21 12:49:01 -0300

    atualizando

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import  multer from "multer";
+import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 
 const usersRouters = Router();
 
@@ -12,13 +13,18 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController;
 const updateUserAvatarController = new UpdateUserAvatarController;
+const profileUserController = new ProfileUserController;
 
 usersRouters.post("/", createUserController.handle);
 
 usersRouters.patch(
-    "/avatar", 
+    "/avatar",
     ensureAuthenticated,
-    uploadAvatar.single("avatar"), 
+    uploadAvatar.single("avatar"),
     updateUserAvatarController.handle);
 
-export { usersRouters};
+usersRouters.get("/profile",
+    ensureAuthenticated,
+    profileUserController.handle);
+
+export { usersRouters };
